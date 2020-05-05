9d395f6ffc162c87327c49e567e11b1b63bc3a2a
Awadhesh Kumar Patel <awadeshpatel212309@gmail.com>
2020-05-05 13:13:50 +0530

    update the project

---
+++ Java16042020/src/main/java/org/stacktrace/arrays/SortArrays.java
@@ -0,0 +1,34 @@
+package org.stacktrace.arrays;
+
+public class SortArrays {
+	public static void main(String[] args) {
+
+		// Initialize the array
+		Integer[] arrays = { 2, 3, 4, 7, 5, 9, 8, 34, 23 };
+		Integer temp = 0;
+
+		// Displaying element of the original arrays
+		System.out.println("Element of the original arrays: ");
+		for (int i = 0; i < arrays.length; i++) {
+			System.out.print(" " + arrays[i]);
+		}
+
+		// Sort the element in acceding order
+		for (int i = 0; i < arrays.length; i++) {
+			for (int j = i + 1; j < arrays.length; j++) {
+				if (arrays[i] < arrays[j]) {
+					temp = arrays[i];
+					arrays[i] = arrays[j];
+					arrays[j] = temp;
+				}
+			}
+		}
+		System.out.println();
+
+		// Displaying the element in after sorting
+		System.out.println(" After sorting the arrays: ");
+		for (int i = 0; i < arrays.length; i++) {
+			System.out.print(" " + arrays[i]);
+		}
+	}
+}
